1e0c167dcb3712bb079c1138e2de5e08da324361
sembori <sembori@yandex.ru>
2023-02-13 20:27:45 +0300

    CreateMethode

--- Program.cs
+++ Program.cs
@@ -1,4 +1,23 @@
 ﻿using static System.Comsole;
 Clear();
 
-string[] array = new string[10];
+string[] massive = new string[10];
+string[] massive = { "-2", "Moscow", "Boo", "24313", "88", "Stop", "98", "youp", "00", "dsdfv" };
+
+string[] SearchElementOfMassive(string[] array)
+{
+    int count = 0;
+    string[] newArray = new string[count];
+    for (int i = 0; i < array.length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            count++;
+        }
+        for (int j = 0; j < count; j++)
+        {
+            newArray[j] = array[i];
+        }
+    }
+
+}
