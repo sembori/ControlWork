3d28b24732cfa6c32989bd19903c53797a9c3b24
sembori <sembori@yandex.ru>
2023-02-13 20:44:05 +0300

    CreateMethode2

--- Program.cs
+++ Program.cs
@@ -1,23 +1,33 @@
-﻿using static System.Comsole;
+﻿using static System.Console;
 Clear();
+string[] massive = { "-2", "Moscow", "Boo", "24313", "88", "Stop", "98", "youp", "00", "dsdfv" };;
+PrintArray(SearchElementOfMassive(massive));
 
-string[] massive = new string[10];
-string[] massive = { "-2", "Moscow", "Boo", "24313", "88", "Stop", "98", "youp", "00", "dsdfv" };
 
 string[] SearchElementOfMassive(string[] array)
 {
+    int j = 0;
     int count = 0;
     string[] newArray = new string[count];
-    for (int i = 0; i < array.length; i++)
+    for (int i = 0; i < array.Length; i++)
     {
         if (array[i].Length <= 3)
         {
-            count++;
-        }
-        for (int j = 0; j < count; j++)
-        {
+         count ++;
             newArray[j] = array[i];
+            j++;
+        
         }
     }
+    return newArray;
 
+}
+void PrintArray(string[] inArray)
+{
+    Write("[");
+    for (int i = 0; i < inArray.Length - 1; i++)
+    {
+        Write($"{inArray[i]},");
+    }
+    Write($"{inArray[inArray.Length - 1]}]");
 }
